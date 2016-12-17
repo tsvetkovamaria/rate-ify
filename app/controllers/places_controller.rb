8fe5645f2ba1d2d8cccfd37ce58140c6f16f129e
Maria <tsvetkovamariia@gmail.com>
2016-12-17 13:46:58 +0600

    create basic CRUD for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,59 @@
 class PlacesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+  before_action :correct_user, only: [:edit, :update, :destroy] 
+
   def index
+    @places = Place.all
   end
 
   def new
+    @place = Place.new
   end
 
   def create
+    @place = Place.new(place_params)
+    if @place.save
+      flash[:success] = "You added a new place"
+      redirect_to @place
+    else
+      flash[:danger] = "Something went wrong"
+      render 'new'
+    end
   end
 
-  def view
+  def show
+    @place = Place.find(params[:id])
   end
 
   def edit
+    @place = Place.find(params[:id])
   end
 
   def update
+    @place = Place.find(params[:id])
+    if @place.update(place_params)
+      flash[:success] = "You added a new place"
+      redirect_to @place
+    else
+      flash[:danger] = "Something went wrong"
+      render 'edit'
+    end
   end
 
   def destroy
+    @place = Place.destroy(params[:id])
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:title, :description, :image, :category_id, :user_id)
+  end
+
+  def correct_user
+    @place = Place.find(params[:id])
+    redirect_to(root_path) unless current_user == @place.user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217055002) do
+ActiveRecord::Schema.define(version: 20161217074239) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,9 +22,15 @@ ActiveRecord::Schema.define(version: 20161217055002) do
     t.string   "title"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["category_id"], name: "index_places_on_category_id"
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
